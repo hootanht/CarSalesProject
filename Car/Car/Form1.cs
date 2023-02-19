1c35768cfea53b587f63ba9d2d93caa2efab6932
kimmya.heidary@yahoo.com <kimmya.heidary@yahoo.com>
2023-02-19 19:48:52 +0330

    initial project

---
+++ Car/Car/Form1.cs
@@ -0,0 +1,15 @@
+namespace Car
+{
+    public partial class Car : Form
+    {
+        public Car()
+        {
+            InitializeComponent();
+        }
+
+        private void Car_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
